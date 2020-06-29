e74afcfc84205fa12afaeffb69716ff26abd9429
Beejay <kdbeejay@gmail.com>
2020-06-29 17:19:17 +0545

    Added Blogs and blogpost

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+	myposts = Blog.objects.all()
+	print(myposts)
+	return render(request, 'blog/index.html', {'myposts': myposts})
+
+def blogpost(request, id):
+	post = Blog.objects.filter(post_id = id)[0]
+	print(post)
+	return render(request, 'blog/blogpost.html', {'post':post})
